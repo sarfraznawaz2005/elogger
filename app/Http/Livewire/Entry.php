34e949c3bd91cf6814be2924861a52b3d500d07b
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-07-29 21:14:18 +0500

    updated

--- app/Http/Livewire/Entry.php
+++ app/Http/Livewire/Entry.php
@@ -60,6 +60,8 @@ class Entry extends Component
     {
         $this->validateOnly($propertyName);
 
+        // TODO: The sorting of todoLists and Todos gets lost due to livewire
+
         if (($propertyName === 'item.project_id') && $this->item['project_id']) {
             $this->todoLists = [];
             $this->todos = [];
--- resources/views/livewire/entry.blade.php
+++ resources/views/livewire/entry.blade.php
@@ -1,7 +1,9 @@
 <div>
 
-    <x-jet-button wire:loading.attr="disabled" wire:click="openModal"
-                  class="bg-green-700 hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+    <x-jet-button
+        wire:loading.attr="disabled"
+        wire:click="openModal"
+        class="bg-green-700 hover:bg-green-800 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
              stroke="currentColor" stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4"/>
