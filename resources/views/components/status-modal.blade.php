bfeadf3afffe925265bd80cdef885643cb6a4723
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-07-30 16:45:57 +0500

    updated

--- resources/views/components/status-modal.blade.php
+++ resources/views/components/status-modal.blade.php
@@ -1,5 +1,25 @@
-@props(['id' => null, 'maxWidth' => null])
+<div class="inline" x-data="{ open: @entangle($attributes->wire('model')).defer, working: false }" x-cloak wire:key="delete-{{ time() }}">
 
-<x-modal :id="$id" :maxWidth="$maxWidth" {{ $attributes }}>
-    {{ $content }}
-</x-modal>
+    <div x-show="open"
+         class="fixed z-50 bottom-0 inset-x-0 px-4 pb-4 sm:inset-0 sm:flex sm:items-center sm:justify-center">
+        <div x-show="open" x-transition:enter="ease-out duration-300" x-transition:enter-start="opacity-0"
+             x-transition:enter-end="opacity-100" x-transition:leave="ease-in duration-200"
+             x-transition:leave-start="opacity-100" x-transition:leave-end="opacity-0"
+             class="fixed inset-0 transition-opacity">
+            <div class="absolute inset-0 bg-gray-500 opacity-75"></div>
+        </div>
+
+        <div x-show="open" x-transition:enter="ease-out duration-300"
+             x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+             x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
+             x-transition:leave="ease-in duration-200"
+             x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
+             x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+             class="relative bg-gray-100 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg sm:w-full">
+
+            <div class="w-full">
+                {{ $content }}
+            </div>
+        </div>
+    </div>
+</div>
