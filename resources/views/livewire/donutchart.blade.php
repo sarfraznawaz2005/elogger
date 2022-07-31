b50697ce06b714a749e503cfc21301322b5b3177
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-07-31 17:47:43 +0500

    updated

--- resources/views/livewire/donutchart.blade.php
+++ resources/views/livewire/donutchart.blade.php
@@ -1,22 +1,22 @@
 <div>
+    <span
+        wire:ignore
+        class="inline-flex rounded-md"
+        title="Current: {{round($monthHours + $pendingHours)}}/{{($workDayCount - user()->holidays_count) * user()->working_hours_count}}">
         <span
-            wire:ignore
-            class="inline-flex rounded-md"
-            title="Current: {{round($monthHours + $pendingHours)}}/{{($workDayCount - user()->holidays_count) * user()->working_hours_count}}">
-            <span
-                class="donutChart hidden"
-                data-peity='{ "fill": ["{{(round($monthHours + $pendingHours)) < (($workDayCount - user()->holidays_count) * user()->working_hours_count) ? '#E2BB29' : '#02C372'}}", "#eeeeee"], "innerRadius": 10, "radius": 15 }'>{{(round($monthHours) + $pendingHours)}}/{{$workDayCount * user()->working_hours_count}}
-            </span>
+            class="donutChart hidden"
+            data-peity='{ "fill": ["{{(round($monthHours + $pendingHours)) < (($workDayCount - user()->holidays_count) * user()->working_hours_count) ? '#E2BB29' : '#02C372'}}", "#eeeeee"], "innerRadius": 10, "radius": 15 }'>{{(round($monthHours) + $pendingHours)}}/{{$workDayCount * user()->working_hours_count}}
         </span>
+    </span>
 
+    <span
+        wire:ignore
+        class="inline-flex rounded-md ml-2"
+        title="Projected: {{round($monthHours) + user()->working_hours_count}}/{{($workDayCount - user()->holidays_count) * user()->working_hours_count}}">
         <span
-            wire:ignore
-            class="inline-flex rounded-md ml-2"
-            title="Projected: {{round($monthHours) + user()->working_hours_count}}/{{($workDayCount - user()->holidays_count) * user()->working_hours_count}}">
-            <span
-                class="donutChart hidden"
-                data-peity='{ "fill": ["{{(round($monthHours) + user()->working_hours_count) < (($workDayCount - user()->holidays_count) * user()->working_hours_count) ? '#E2BB29' : '#02C372'}}", "#eeeeee"], "innerRadius": 10, "radius": 15 }'>{{(round($monthHours) + user()->working_hours_count)}}/{{$workDayCount * user()->working_hours_count}}
-            </span>
+            class="donutChart hidden"
+            data-peity='{ "fill": ["{{(round($monthHours) + user()->working_hours_count) < (($workDayCount - user()->holidays_count) * user()->working_hours_count) ? '#E2BB29' : '#02C372'}}", "#eeeeee"], "innerRadius": 10, "radius": 15 }'>{{(round($monthHours) + user()->working_hours_count)}}/{{$workDayCount * user()->working_hours_count}}
+        </span>
     </span>
 </div>
 
