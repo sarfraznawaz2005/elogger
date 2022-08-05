067bc7233eec69ec453f0f95b88c7ddac56ae088
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-06 02:04:21 +0500

    updated

--- resources/views/livewire/dashboard/stats.blade.php
+++ resources/views/livewire/dashboard/stats.blade.php
@@ -1,26 +1,43 @@
 <div class="bg-white overflow-hidden shadow-md sm:rounded-lg p-4 mb-6">
 
     <div class="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-6 w-full">
-        <div class="flex items-center relative p-4 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
-            <x-jet-section-title>
-                <x-slot name="title">Work Day ({{date('d F Y')}})</x-slot>
-                <x-slot name="description"><strong class="text-2xl">{{$workDays}}</strong></x-slot>
-            </x-jet-section-title>
+
+        <div class="flex items-center relative p-2 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
+            <div class="w-full text-center">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-sm font-medium text-gray-900 sm:text-lg">Work Day ({{date('d F Y')}})</h3>
+
+                    <p class="mt-1 text-sm text-gray-600">
+                        <strong class="text-2xl">{{$workDays}}</strong>
+                    </p>
+                </div>
+            </div>
         </div>
 
-        <div class="flex items-center relative p-4 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
-            <x-jet-section-title>
-                <x-slot name="title">Hours Uploaded</x-slot>
-                <x-slot name="description"><strong class="text-2xl">{{number_format($hoursUploaded, 2)}} of {{$hoursTotal}}</strong></x-slot>
-            </x-jet-section-title>
+        <div class="flex items-center relative p-2 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
+            <div class="w-full text-center">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-sm font-medium text-gray-900 sm:text-lg">Hours Uploaded</h3>
+
+                    <p class="mt-1 text-sm text-gray-600">
+                        <strong class="text-2xl">{{number_format($hoursUploaded, 2)}} of {{$hoursTotal}}</strong>
+                    </p>
+                </div>
+            </div>
         </div>
 
-        <div class="flex items-center relative p-4 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
-            <x-jet-section-title>
-                <x-slot name="title">Hours Projected</x-slot>
-                <x-slot name="description"><strong class="text-2xl">{{number_format($hoursProjected, 2)}} of {{$hoursTotal}}</strong></x-slot>
-            </x-jet-section-title>
+        <div class="flex items-center relative p-2 w-full bg-gray-100 rounded-lg overflow-hidden shadow-md">
+            <div class="w-full text-center">
+                <div class="px-4 sm:px-0">
+                    <h3 class="text-sm font-medium text-gray-900 sm:text-lg">Hours Projected</h3>
+
+                    <p class="mt-1 text-sm text-gray-600">
+                        <strong class="text-2xl">{{number_format($hoursProjected, 2)}} of {{$hoursTotal}}</strong>
+                    </p>
+                </div>
+            </div>
         </div>
+
     </div>
 
 
