dbce23b7d8f6f88609c985a5d868db1091cb5b83
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-05 14:15:09 +0500

    updated

--- resources/views/components/flash.blade.php
+++ resources/views/components/flash.blade.php
@@ -7,7 +7,7 @@
      x-show="show && message"
      x-init="setTimeout(() => show = false, 5000)">
 
-    <div class="fixed right-4 top-4 z-50 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg animate-in slide-in-from-right duration-500">
+    <div class="fixed left-4 bottom-4 z-50 rounded-lg overflow-hidden shadow-xl transform transition-all sm:max-w-lg duration-500 animate-in slide-in-from-left">
 
         <div class="bg-blue-500 shadow border border-blue-500 flex items-center text-center" role="alert" x-show="style == 'info'">
             <div class="p-3 text-sm text-white break-words flex items-center">
