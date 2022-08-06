b1f03d0efa00f17e4cc835c70090a6e1239badec
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-06 23:22:42 +0500

    updated

--- app/Http/Livewire/Users/IndexUsers.php
+++ app/Http/Livewire/Users/IndexUsers.php
@@ -9,8 +9,16 @@ use Livewire\Component;
 
 class IndexUsers extends Component
 {
+    // doing this because this page takes some time to load due to calculations
+    public bool $loading = true;
+
     public function render(): Factory|View|Application
     {
         return view('livewire.users.index');
+    }
+
+    public function load(): void
+    {
+        $this->loading = false;
     }
 }
--- resources/views/livewire/users/index.blade.php
+++ resources/views/livewire/users/index.blade.php
@@ -1,5 +1,15 @@
-<div wire:ignore>
-    <x-panel title="Users">
-        <livewire:data-tables.users-data-table />
-    </x-panel>
+<div wire:init="load">
+
+    <x-status-modal wire:model="loading">
+        Please wait while we are fetching user data...
+    </x-status-modal>
+
+    @if (!$loading)
+        <div wire:ignore>
+            <x-panel title="Users">
+                <livewire:data-tables.users-data-table/>
+            </x-panel>
+        </div>
+    @endif
+
 </div>
