ad68593ccaf922c24587d15fce16dd38017d885a
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-07 18:00:08 +0500

    updated

--- resources/views/components/bc-connection-checker.blade.php
+++ resources/views/components/bc-connection-checker.blade.php
@@ -9,7 +9,7 @@
                 <p class="mb-2">We are unable to communicate with Basecamp API, make sure you are connected to internet & your settings are correct.</p>
                 <p class="mb-4">If you think this is wrong, try refreshing data using button below.</p>
 
-                <x-jet-button x-on:click="window.livewire.emit('refreshClicked')" wire:loading.attr="disabled" data-title="Refresh Data" class="bg-blue-600 px-2.5 hover:bg-blue-800 mr-2 border-0">
+                <x-jet-button x-on:click="window.livewire.emit('refreshClicked')" wire:loading.attr="disabled" data-title="Refresh Data" class="bg-gray-700 px-2.5 hover:bg-blue-600 mr-2 border-0">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/>
                     </svg>
