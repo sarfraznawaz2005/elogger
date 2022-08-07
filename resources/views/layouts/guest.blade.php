2a3c3c02b2553cb491e30fc8db6487f10a2026bb
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-07 23:41:36 +0500

    updated

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -20,8 +20,8 @@
     <body class="font-sans antialiased">
         <x-jet-banner />
 
-        <div class="min-h-screen bg-gray-100">
-            <nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+        <div class="min-h-screen bg-gray-200">
+            <nav x-data="{ open: false }" class="bg-white border-b border-gray-200">
                 <!-- Primary Navigation Menu -->
                 <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                     <div class="flex justify-between h-16">
