052e9857756819f9348253286bf5d00d9d44b3a2
Sarfraz Ahmed <sarfraz@onsupport.com>
2022-08-11 15:32:46 +0500

    update

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -3,4 +3,4 @@ https://github.com/spatie/emoji (http://unicode.org/emoji/charts/full-emoji-list
 404s Solution:
 Possible solution of 404s: https://stackoverflow.com/questions/69553897/laravel-livewire-how-to-customize-the-global-message-url
 
-But if issue still comes, try to use $id instead of "Todo $todo" in arguments of functions inside Entry component.
+But if issue still comes, try to use $id instead of "Todo $todo" in arguments of functions inside Entry component or use null check and inform user.
