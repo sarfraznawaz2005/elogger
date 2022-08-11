294687e58541dec42865dbc82839619ee03cb9b5
Sarfraz Ahmed <sarfraz@onsupport.com>
2022-08-11 18:45:57 +0500

    update

--- resources/views/components/table-totals.blade.php
+++ resources/views/components/table-totals.blade.php
@@ -3,7 +3,7 @@
         <x-label-segmented
             class="font-semibold mr-0"
             color="blue"
-            label="Pending Total"
+            label="Pending Total All Months"
             value="{{number_format(user()->pendingTodosHours(), 2)}}"/>
     @endif
 
@@ -11,7 +11,7 @@
         <x-label-segmented
             class="font-semibold mr-0"
             color="blue"
-            label="Uploaded Total"
+            label="Uploaded Total All Months"
             value="{{number_format(user()->postedTodosHours(), 2)}}"/>
     @endif
 </div>
