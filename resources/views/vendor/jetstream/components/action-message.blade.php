6e35869cc7138dea339a97b52c6e806f6f73d898
Sarfraz Ahmed <sarfraz@onsupport.com>
2022-08-11 15:49:40 +0500

    update

--- resources/views/vendor/jetstream/components/action-message.blade.php
+++ resources/views/vendor/jetstream/components/action-message.blade.php
@@ -1,7 +1,7 @@
 @props(['on'])
 
 <div x-data="{ shown: false, timeout: null }"
-    x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; const notyf = new Notyf({duration: 5000, position: {x: 'right', y: 'bottom'}, ripple: false, dismissible: true}); notyf.success('Saved Successfully!'); Livewire.emit('event-entries-updated'); timeout = setTimeout(() => { shown = false }, 2000);  })"
+    x-init="@this.on('{{ $on }}', () => { clearTimeout(timeout); shown = true; timeout = setTimeout(() => { shown = false }, 2000);  })"
     x-show.transition.out.opacity.duration.1500ms="shown"
     x-transition:leave.opacity.duration.1500ms
     style="display: none;"
