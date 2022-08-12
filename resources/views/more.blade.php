29cdc6cf12f67d506d0723105f5db410394f0239
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-12 21:48:27 +0500

    updated

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -1,9 +1,5 @@
 view my logs should show hours frm sat/sun in blue if there are any
 
-https://elogger.eteamprojects.com/livewire/message/entries.entry
-https://elogger.eteamprojects.com/vendor/livewire/livewire.js?id=c69d0f2801c01fcf8166
 --------------------------------------------------------------------------------
 404s Solution:
 Possible solution of 404s: https://stackoverflow.com/questions/69553897/laravel-livewire-how-to-customize-the-global-message-url
-
-But if issue still comes, try to use $id instead of "Todo $todo" in arguments of functions inside Entry component or use null check and inform user.
