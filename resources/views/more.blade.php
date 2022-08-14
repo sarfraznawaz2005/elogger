43df691ff38596e188c79c328c709efa246f17b2
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-14 15:57:18 +0500

    updated

--- resources/views/more.blade.php
+++ resources/views/more.blade.php
@@ -1,7 +1,5 @@
 
 
-
-
 --------------------------------------------------------------------------------
 404s Solution:
 Possible solution of 404s: https://stackoverflow.com/questions/69553897/laravel-livewire-how-to-customize-the-global-message-url
