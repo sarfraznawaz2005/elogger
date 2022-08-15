35acdc04330062151d84ade455cb77ed480dd927
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-15 17:07:29 +0500

    updated

--- app/Traits/EntriesTableCommonTrait.php
+++ app/Traits/EntriesTableCommonTrait.php
@@ -24,7 +24,7 @@ trait EntriesTableCommonTrait
     {
         $columns = [
 
-            Column::name('id')->hide()->label('ID')->defaultSort('desc'),
+            //Column::name('id')->hide()->label('ID')->defaultSort('desc'),
 
             DateColumn::name('dated')->label('Date'),
 
