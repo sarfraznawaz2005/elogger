b89d23cd44367bfbd6ab6635a79c5bfc0ae6207a
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-17 15:46:29 +0500

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,6 +8,9 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+
     @vite(['resources/js/app.js'])
 
     <livewire:styles />
