bf63f439539193b4784456b5f8e8b163279fca1c
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-18 00:10:31 +0500

    updated

--- resources/views/components/table-actions-post-delete-buttons.blade.php
+++ resources/views/components/table-actions-post-delete-buttons.blade.php
@@ -97,11 +97,11 @@
             <div class="md:flex items-center justify-start" wire:ignore>
                 <div class="flex items-center mr-2 py-2 px-4 rounded border border-gray-300 bg-white justify-between">
 
-                    <input x-ref="selectAll" id="select-all-checkbox" type="checkbox"
+                    <input x-ref="selectAll" id="select-all-checkbox"
+                           type="checkbox"
                            class="w-4 h-4 focus:outline-none">
 
-                    <label for="select-all-checkbox"
-                           class="ml-2 text-sm font-medium text-gray-900 select-none">
+                    <label for="select-all-checkbox" class="ml-2 text-sm font-medium text-gray-900 select-none">
                         Select All
                     </label>
                 </div>
@@ -125,9 +125,14 @@
                         wire:loading.attr="disabled"
                         class="bg-green-700 hover:bg-green-800">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor"
+                             stroke-width="2">
+
+                            <path stroke-linecap="round"
+                                  stroke-linejoin="round"
                                   d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"/>
                         </svg>
 
@@ -143,10 +148,16 @@
                         wire:loading.attr="disabled"
                         class="bg-red-700 hover:bg-red-800">
 
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
-                             stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                  d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2"
+                             fill="none"
+                             viewBox="0 0 24 24"
+                             stroke="currentColor"
+                             stroke-width="2">
+
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                         </svg>
 
                         {{ __('Delete Selected') }}
@@ -164,15 +175,21 @@
                 x-on:click="sendBrowserEvent('confirm', 'deleteAllPosted', null, 'Are you sure you want to delete all posted entries ?')"
                 wire:loading.attr="disabled"
                 class="bg-red-700 hover:bg-red-800">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2" fill="none" viewBox="0 0 24 24"
-                     stroke="currentColor" stroke-width="2">
-                    <path stroke-linecap="round" stroke-linejoin="round"
-                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
+
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 mr-2"
+                     fill="none"
+                     viewBox="0 0 24 24"
+                     stroke="currentColor"
+                     stroke-width="2">
+
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                 </svg>
 
                 {{ __('Delete All') }}
             </x-jet-danger-button>
-
         </div>
     @endif
 
