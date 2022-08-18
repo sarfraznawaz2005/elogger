eb78e6e2dbee352bd365bfd72e57f4d9301677b1
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-18 13:40:44 +0500

    updated

--- app/Traits/EntriesTableCommonTrait.php
+++ app/Traits/EntriesTableCommonTrait.php
@@ -5,6 +5,7 @@ namespace App\Traits;
 use App\Models\Project;
 use App\Models\Todo;
 use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Str;
 use Mediconesystems\LivewireDatatables\Column;
 use Mediconesystems\LivewireDatatables\DateColumn;
@@ -19,10 +20,20 @@ trait EntriesTableCommonTrait
 
             //Column::name('id')->hide()->label('ID')->defaultSort('desc'),
 
-            DateColumn::name('dated')->label('Date')
-                ->width('130px')
-                ->alignCenter()
-                ->format('d M Y'),
+            DateColumn::callback(['dated'], static function ($date) {
+                $color = 'gray';
+                $date = date('d M Y', strtotime($date));
+
+                if (Carbon::parse($date)->isToday()) {
+                    $color = 'green';
+                }
+
+                return <<<html
+                    <span class="hours bg-$color-200 text-gray-700 text-md font-semibold px-2 py-1 rounded inline-block w-24">
+                        $date
+                    </span>
+                html;
+            })->label('Date')->width('1px')->alignCenter(),
 
             // causing search issue as well. tbf
             Column::callback('project_id', function ($project_id) {
@@ -61,20 +72,36 @@ trait EntriesTableCommonTrait
                 html;
             })->label('Description')->alignCenter(),
 
-            TimeColumn::name('time_start')->label('Time Start')->width('125px')->alignCenter(),
-            TimeColumn::name('time_end')->label('Time End')->width('125px')->alignCenter(),
+            TimeColumn::callback(['time_start'], static function ($time_start) {
+                $time = date('h:i A', strtotime($time_start));
+
+                return <<<html
+                    <span class="hours bg-yellow-200 text-gray-700 text-md font-semibold px-2 py-1 rounded inline-block w-20">
+                        $time
+                    </span>
+                html;
+            })->label('Time Start')->width('1px')->alignCenter(),
+
+            TimeColumn::callback(['time_end'], static function ($time_end) {
+                $time = date('h:i A', strtotime($time_end));
+
+                return <<<html
+                    <span class="hours bg-yellow-200 text-gray-700 text-md font-semibold px-2 py-1 rounded inline-block w-20">
+                        $time
+                    </span>
+                html;
+            })->label('Time End')->width('1px')->alignCenter(),
 
             NumberColumn::callback(['dated', 'time_start', 'time_end'], static function ($dated, $time_start, $time_end) {
                 $hours = getBCHoursDiff($dated, $time_start, $time_end);
 
-                /** @noinspection ALL */
                 return <<<html
                     <span class="hours bg-green-200 text-gray-700 text-md font-semibold px-2 py-1 rounded inline-block w-16">
                         $hours
                     </span>
                 html;
 
-            })->label('Total')->width('110px')->alignCenter(),
+            })->label('Total')->width('1px')->alignCenter(),
 
             Column::callback(['id', 'id'], function ($id) {
                 return view('components.table-actions-entry', ['id' => $id, 'isPendingTable' => $this->isPendingTable]);
@@ -84,13 +111,12 @@ trait EntriesTableCommonTrait
         // add select checkbox to pending table only
         if ($this->isPendingTable) {
             array_unshift($columns, Column::callback(['id'], static function ($id) {
-                /** @noinspection ALL */
                 return <<<html
                     <div>
                         <input type="checkbox" class="check-entry" value="$id"/>
                     </div>
                 html;
-            })->alignCenter()->width('50px')->excludeFromExport());
+            })->alignCenter()->width('1px')->excludeFromExport());
         }
 
         return $columns;
