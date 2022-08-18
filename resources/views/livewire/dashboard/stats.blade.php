c98693138ebfb4f4ea10ff19c2bfb41f44e9432c
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-18 17:23:31 +0500

    updated

--- resources/views/livewire/dashboard/stats.blade.php
+++ resources/views/livewire/dashboard/stats.blade.php
@@ -5,7 +5,7 @@
         <div class="flex items-center relative p-3 w-full bg-gray-200 rounded shadow-md">
             <div class="w-full text-center">
                 <div class="px-4 sm:px-0">
-                    <h3 class="text-sm font-medium text-gray-900 sm:text-lg">Work Day ({{date('d F Y')}})</h3>
+                    <h3 class="text-sm font-medium text-gray-900 sm:text-lg">Work Day ({{date('d M Y')}})</h3>
 
                     <p class="mt-2 text-sm text-gray-600">
                         <strong class="md:text-2xl sm:text-xs">{{$workDays}}</strong>
