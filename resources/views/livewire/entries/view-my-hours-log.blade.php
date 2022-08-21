2033cf19966873f418d9564797eae885e4d6f057
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-21 11:49:45 +0500

    updated

--- resources/views/livewire/entries/view-my-hours-log.blade.php
+++ resources/views/livewire/entries/view-my-hours-log.blade.php
@@ -13,7 +13,7 @@
             <path stroke-linecap="round" stroke-linejoin="round" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
         </svg>
 
-        {{ __('View My Hours Log') }}
+        {{ __('My Hours Log') }}
     </button>
 
     <x-jet-dialog-modal wire:model="isModalOpen" maxWidth="sm">
