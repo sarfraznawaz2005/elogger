7b25e34bd9ba260262aed07dcbd0b8930ef88f72
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-22 15:30:42 +0500

    updated

--- deploy.bat
+++ deploy.bat
@@ -1,1 +1,11 @@
-php floyer deploy --ansi
+rem php floyer deploy --ansi
+
+rem push code
+git pull
+git status
+git add .
+git commit -am "updated"
+git push
+
+rem deploy on server
+curl -I https://elogger.eteamprojects.com/deploy
