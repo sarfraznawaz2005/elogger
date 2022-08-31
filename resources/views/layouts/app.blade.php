221a5d4391c7122e94d545403d95afc6e36527d4
Sarfraz Ahmed <sarfraznawaz2005@gmail.com>
2022-08-31 20:13:43 +0500

    updated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
     @vite(['resources/js/app.js'])
